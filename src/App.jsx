2ce67b28a2bb1b4da2708c52e007d9d220aba2e0
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2024-09-10 22:38:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
       </div>
       <div className="social-icons-1">
         <a
-          href="https://github.com/pranjal-sachan"
+          href="https://github.com/pranjalsachan9935"
           target="_blank"
           rel="noopener noreferrer"
         >
