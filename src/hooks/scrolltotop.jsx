52337c15c9983f55f28010c6fd7670916ac1d96f
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2024-10-05 23:30:04 +0530

    Update scrolltotop.jsx

--- src/hooks/scrolltotop.jsx
+++ src/hooks/scrolltotop.jsx
@@ -3,12 +3,14 @@ import { useEffect } from 'react';
 const useScrollToTop = () => {
   useEffect(() => {
     // Added a slight delay to ensure all elements are rendered before scrolling
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
       });
     }, 100); 
+    
+    return () => clearTimeout(timer);
   }, []);
 };
 
