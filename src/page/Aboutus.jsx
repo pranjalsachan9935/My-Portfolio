0c20d29b8f8f9717a79512b541e42a289b0e14e2
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2024-10-13 20:23:41 +0530

    Update Aboutus.jsx

--- src/page/Aboutus.jsx
+++ src/page/Aboutus.jsx
@@ -4,12 +4,8 @@ import profileImage from './pranjal.png';
 
 
 function About() {
-  console.log('about is rendered');//like this
-
-
-
   return (
-    <div className='outer-container'>
+    <div className='about-outer-container'>
       <div className='left-section'>
       <img src={profileImage} alt="Pranjal Sachan" className="profile-image" /> 
       </div>
@@ -29,7 +25,7 @@ function About() {
   <span>n</span>
   <span>g</span>
 </span> Student</p>
-        <p className='bio-text'>I am a B.Tech Software Engineering student with a passion for technology and coding.
+        <p className='about-bio-text'>I am a B.Tech Software Engineering student with a passion for technology and coding.
         I am a dedicated software engineering student who loves exploring new technologies and building applications that make a difference. My interests include web development, programming languages, and emerging tech trends.</p>
       </div>
     </div>
